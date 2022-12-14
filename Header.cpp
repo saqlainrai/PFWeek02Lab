c7a6bc0555138e7bb051cdc8bf972e5fe005092d
Saqlain Mansab <saqlainmansab786@gmail.com>
2022-12-14 12:10:44 -0800

    Task Folders for PF Lab

---
+++ Header.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+main(){
+	cout<<"*****************************************************************"<<endl;
+	cout<<"*                                                               *"<<endl;
+	cout<<"*                                                               *"<<endl;
+	cout<<"*                 University of Engineering and                 *"<<endl;
+	cout<<"*                                                               *"<<endl;
+	cout<<"*                                                               *"<<endl;
+	cout<<"*                     Technology, Lahore                        *"<<endl;
+	cout<<"*                                                               *"<<endl;
+	cout<<"*                                                               *"<<endl;
+	cout<<"*****************************************************************"<<endl;
+	
+}
